2338d171faa04fa72f2eb9317e8bf19c03a1fe8a
Dmytro Leonov <leonovdmitriy02@gmail.com>
2026-03-08 16:08:52 +0200

    feat: add tokens for quoted and unquoted strings

--- packages/parser/src/keywords/createKeywords.ts
+++ packages/parser/src/keywords/createKeywords.ts
@@ -1,51 +1,51 @@
 import { createToken, Lexer, type TokenType } from 'chevrotain';
 import { SearchQlError } from '@/errors/searchQlError.js';
 
-export const Colon = createToken({
-  name: 'Colon',
-  pattern: /:/,
-});
-export const StringLiteral = createToken({
-  name: 'StringLiteral',
-  pattern: /"(:?[^\\"]|\\(:?[bfnrtv"\\/]|u[0-9a-fA-F]{4}))*"/,
-});
-export const NumberLiteral = createToken({
-  name: 'NumberLiteral',
-  pattern: /-?(0|[1-9]\d*)(\.\d+)?([eE][+-]?\d+)?/,
-});
-export const Keyword = createToken({
-  name: 'Keyword',
-  pattern: /[_A-Za-z][_A-Za-z0-9]*/,
-});
-export const True = createToken({
-  name: 'True',
-  pattern: /true/,
-  longer_alt: Keyword,
-});
-export const False = createToken({
-  name: 'False',
-  pattern: /false/,
-  longer_alt: Keyword,
-});
 export const WhiteSpace = createToken({
   name: 'WhiteSpace',
   pattern: /[ \t\n\r]+/,
   group: Lexer.SKIPPED,
 });
+export const Colon = createToken({
+  name: 'Colon',
+  pattern: /:/,
+});
+export const NumberLiteral = createToken({
+  name: 'NumberLiteral',
+  pattern: /-?(0|[1-9]\d*)(\.\d+)?([eE][+-]?\d+)?/,
+});
+export const UnquotedString = createToken({
+  name: 'UnquotedString',
+  pattern: /(?:\\.|[^\s():~='"-])(?:\\.|[^\s():~='"])*/,
+});
+export const QuotedString = createToken({
+  name: 'QuotedString',
+  pattern: /(['"])(?:\\.|(?!\1)[^\\])*\1/,
+});
+export const True = createToken({
+  name: 'True',
+  pattern: /true/,
+  longer_alt: UnquotedString,
+});
+export const False = createToken({
+  name: 'False',
+  pattern: /false/,
+  longer_alt: UnquotedString,
+});
 export const Not = createToken({
   name: 'Not',
   pattern: /not/,
-  longer_alt: Keyword,
+  longer_alt: UnquotedString,
 });
 export const Or = createToken({
   name: 'Or',
   pattern: /or/,
-  longer_alt: Keyword,
+  longer_alt: UnquotedString,
 });
 export const And = createToken({
   name: 'And',
   pattern: /and/,
-  longer_alt: Keyword,
+  longer_alt: UnquotedString,
 });
 export const LParen = createToken({
   name: 'LParen',
