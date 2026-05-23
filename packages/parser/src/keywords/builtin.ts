b133fc35afc616e22c7f925d1c929375fbd9df0c
Dmytro Leonov <leonovdmitriy02@gmail.com>
2026-05-23 18:12:40 +0300

    fix: update regexes and remove unnecessary tokens

--- packages/parser/src/keywords/builtin.ts
+++ packages/parser/src/keywords/builtin.ts
@@ -9,27 +9,13 @@ export const Colon = createToken({
   name: 'Colon',
   pattern: /:/,
 });
-export const NumberLiteral = createToken({
-  name: 'NumberLiteral',
-  pattern: /-?(0|[1-9]\d*)(\.\d+)?([eE][+-]?\d+)?/,
-});
 export const UnquotedString = createToken({
   name: 'UnquotedString',
-  pattern: /(?:\\.|[^\s():~='"-])(?:\\.|[^\s():~='"])*/,
+  pattern: /(?:\\\\|\\[\s!&|():=~'"\\]|[^\s!&|():=~'"\\])+/,
 });
 export const QuotedString = createToken({
   name: 'QuotedString',
   pattern: /(['"])(?:\\.|(?!\1)[^\\])*\1/,
-});
-export const True = createToken({
-  name: 'True',
-  pattern: /true/,
-  longer_alt: UnquotedString,
-});
-export const False = createToken({
-  name: 'False',
-  pattern: /false/,
-  longer_alt: UnquotedString,
 });
 export const Null = createToken({
   name: 'Null',
@@ -38,17 +24,17 @@ export const Null = createToken({
 });
 export const Not = createToken({
   name: 'Not',
-  pattern: /not/,
+  pattern: /!/,
   longer_alt: UnquotedString,
 });
 export const Or = createToken({
   name: 'Or',
-  pattern: /or/,
+  pattern: /\|/,
   longer_alt: UnquotedString,
 });
 export const And = createToken({
   name: 'And',
-  pattern: /and/,
+  pattern: /&/,
   longer_alt: UnquotedString,
 });
 export const LParen = createToken({
