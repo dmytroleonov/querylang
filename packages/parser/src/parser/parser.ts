8e0424e24dec448a5e770e686273aa99217de280
Dmytro Leonov <leonovdmitriy02@gmail.com>
2026-05-31 04:50:06 +0300

    feat: type-based operations

--- packages/parser/src/parser/parser.ts
+++ packages/parser/src/parser/parser.ts
@@ -204,3 +204,134 @@ export function createParser<TKeywords extends CreateKeywordInput>(
     },
   };
 }
+
+type LRangeOp<
+  TConfig extends TKeywordConfig,
+  TKeyword extends keyof TConfig,
+> = {
+  op: 'L_RANGE';
+  min: TConfig[TKeyword];
+};
+
+type RRangeOp<
+  TConfig extends TKeywordConfig,
+  TKeyword extends keyof TConfig,
+> = {
+  op: 'R_RANGE';
+  max: TConfig[TKeyword];
+};
+
+type FullRangeOp<
+  TConfig extends TKeywordConfig,
+  TKeyword extends keyof TConfig,
+> = {
+  op: 'FULL_RANGE';
+  min: TConfig[TKeyword];
+  max: TConfig[TKeyword];
+};
+
+type EqOp<TConfig extends TKeywordConfig, TKeyword extends keyof TConfig> = {
+  op: 'EQ';
+  value: TConfig[TKeyword];
+};
+
+type LtOp<TConfig extends TKeywordConfig, TKeyword extends keyof TConfig> = {
+  op: 'LT';
+  value: TConfig[TKeyword];
+};
+
+type LteOp<TConfig extends TKeywordConfig, TKeyword extends keyof TConfig> = {
+  op: 'LTE';
+  value: TConfig[TKeyword];
+};
+
+type GtOp<TConfig extends TKeywordConfig, TKeyword extends keyof TConfig> = {
+  op: 'GT';
+  value: TConfig[TKeyword];
+};
+
+type GteOp<TConfig extends TKeywordConfig, TKeyword extends keyof TConfig> = {
+  op: 'GTE';
+  value: TConfig[TKeyword];
+};
+
+type ILikeOp<TConfig extends TKeywordConfig, TKeyword extends keyof TConfig> = {
+  op: 'ILIKE';
+  value: TConfig[TKeyword];
+};
+
+type LikeOp<TConfig extends TKeywordConfig, TKeyword extends keyof TConfig> = {
+  op: 'LIKE';
+  value: TConfig[TKeyword];
+};
+
+type StringOp<TConfig extends TKeywordConfig, TKeyword extends keyof TConfig> =
+  | ILikeOp<TConfig, TKeyword>
+  | LikeOp<TConfig, TKeyword>
+  | EqOp<TConfig, TKeyword>;
+
+type NumberOp<TConfig extends TKeywordConfig, TKeyword extends keyof TConfig> =
+  | LRangeOp<TConfig, TKeyword>
+  | RRangeOp<TConfig, TKeyword>
+  | FullRangeOp<TConfig, TKeyword>
+  | EqOp<TConfig, TKeyword>
+  | LtOp<TConfig, TKeyword>
+  | LteOp<TConfig, TKeyword>
+  | GtOp<TConfig, TKeyword>
+  | GteOp<TConfig, TKeyword>;
+
+type BooleanOp<
+  TConfig extends TKeywordConfig,
+  TKeyword extends keyof TConfig,
+> = EqOp<TConfig, TKeyword>;
+
+type Op<
+  TConfig extends TKeywordConfig,
+  TKeyword extends keyof TConfig,
+> = TConfig[TKeyword] extends string
+  ? StringOp<TConfig, TKeyword>
+  : TConfig[TKeyword] extends number
+    ? NumberOp<TConfig, TKeyword>
+    : TConfig[TKeyword] extends boolean
+      ? BooleanOp<TConfig, TKeyword>
+      : never;
+
+type KeywordExpression<
+  TConfig extends TKeywordConfig,
+  TKeyword extends keyof TConfig,
+> = {
+  type: 'KEYWORD';
+} & { [K in TKeyword]: { keyword: K; value: Op<TConfig, K> } }[TKeyword];
+
+type AndExpression<
+  TConfig extends TKeywordConfig,
+  TKeyword extends keyof TConfig,
+> = {
+  type: 'AND';
+  children: Expression<TConfig, TKeyword>[];
+};
+
+type OrExpression<
+  TConfig extends TKeywordConfig,
+  TKeyword extends keyof TConfig,
+> = {
+  type: 'OR';
+  children: Expression<TConfig, TKeyword>[];
+};
+
+type TKeywordConfig = Record<string, number | string | boolean>;
+
+type Expression<
+  TConfig extends TKeywordConfig,
+  TKeyword extends keyof TConfig = keyof TConfig,
+> =
+  | OrExpression<TConfig, TKeyword>
+  | AndExpression<TConfig, TKeyword>
+  | KeywordExpression<TConfig, TKeyword>;
+
+type Empty = { type: 'EMPTY' };
+
+export type Ast<
+  TConfig extends TKeywordConfig,
+  TKeyword extends keyof TConfig = keyof TConfig,
+> = Expression<TConfig, TKeyword> | Empty;
