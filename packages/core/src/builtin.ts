c19c1390121e2e0dc7de0acae97750be7c510fbe
Dmytro Leonov <leonovdmitriy02@gmail.com>
2026-06-06 20:35:26 +0300

    fix: disallow `\` at the end of the input

--- packages/core/src/builtin.ts
+++ packages/core/src/builtin.ts
@@ -23,11 +23,12 @@ export const NumberValue = createToken({
 // matches any character escaped and doesnt' allow the following without escaping:
 // \s regex
 // !, &, |, (, ), :, =, ~, ', ", ., >, <
+// \ at the end of the input
 export const Value = createToken({
   name: 'value',
   pattern:
     // biome-ignore lint/suspicious/noControlCharactersInRegex: chevrotain workaround
-    /(?:\\.|[\u0000-\u0008\u000e-\u001f\u0023-\u0025\u002a-\u002d\u002f-\u0039\u003b\u003f\u003e-\u007b\u007d\u007f-\u009f\u00a1-\u167f\u1681-\u1fff\u200b-\u2027\u202a-\u202e\u2030-\u205e\u2026-\u2fff\u3001-\ufefe\uff00-\uffff])+/,
+    /(?:\\.|[\u0000-\u0008\u000e-\u001f\u0023-\u0025\u002a-\u002d\u002f-\u0039\u003b\u003f\u003e-\u005b\u005d-\u007b\u007d\u007f-\u009f\u00a1-\u167f\u1681-\u1fff\u200b-\u2027\u202a-\u202e\u2030-\u205e\u2026-\u2fff\u3001-\ufefe\uff00-\uffff])+/,
   categories: AnyValue,
 });
 export const QuotedValue = createToken({
